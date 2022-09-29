9065d8f8ab1d44c04fcf754ddb8c1b7e930d142a
Hussein Elfakharany <fakharany.hussein@gmail.com>
2022-09-29 11:16:08 +0200

    Free standard pool

--- src/mem_alloc_standard_pool.c
+++ src/mem_alloc_standard_pool.c
@@ -22,24 +22,38 @@ std_pool_placement_policy_t std_pool_policy = DEFAULT_STDPOOL_POLICY;
 //Size = total size (including header and footer
 void init_standard_pool(mem_pool_t *p, size_t size, size_t min_request_size, size_t max_request_size) {
     //Call my_mmap & set the head of the pool
-    char* firstAddressOfPool = (char*) my_mmap(size) ;  //cast en mem_std_freeblock?
-    p->first_free = firstAddressOfPool ;
-    p->start =  firstAddressOfPool ;
-    p->end = firstAddressOfPool + size ;
+    char* region = (char*) my_mmap(size);  //cast en mem_std_freeblock?
+
+    p->start = region;
+    p->end = region+size; //&region[size]
+
+    p->first_free = region;
 
     //Initialize the list of free blocs
-    mem_std_free_block_t* firstBlock = p->first_free ; //Facilitate access
+    mem_std_free_block_t* firstBlock = p->first_free; //Facilitate access
 
-    /**Initialize first free bloc**/
+
+    //Initialize the first free block
+    //Header & footer setup ( same function for the two of them)
+
     /**Header**/
-    mem_std_block_header_footer_t* header = firstAddressOfPool  ;  //Footer is 8 bytes long
-    initializeHeader(header, size, true) ;
+    set_block_size((mem_std_block_header_footer_t*)&(firstBlock->header), size); //set_block_size(firstAdressOfPool + 8), size) ; //Optimized
+    set_block_free((mem_std_block_header_footer_t*)&(firstBlock->header));
 
+    //We don't know yet the size of the payload, except in the initialization since we know the size of the pool and there is only one block.
+    //Inside the 'malloc' functiontion, we need to know the size of the payload in order to know the starting adress of the footer
+    
     /**Footer**/
-    mem_std_block_header_footer_t* footer = firstAddressOfPool + (size - 8 ) ;  //Footer is 8 bytes long
-    initializeHeader(addressFooter, size, true) ;
+    char* footerAddress = region+(size-8);  //Footer is 8 bytes long*
+    //mem_std_block_headerfooter_t* footerPointer = firstAdressOfPool + size -8  ;
+    // = -8 * size de mem std header  ou = -8 octet
+    // Se renseigner sur comment fonctionne le comportement des operateur selon le type static du pointeur a droit et a gauche de l'asignement
+    //if char* is passed as parameter instead of any other type*, i'ts probably cast, '->' is an operator which compute the size of the data before the field and modify the pointer ?
 
-    //List management
+    set_block_size((mem_std_block_header_footer_t*)footerAddress, size);
+    set_block_free((mem_std_block_header_footer_t*)footerAddress);
+    printf("size:%d", sizeof(mem_std_free_block_t));
+    /**List management**/
     firstBlock->next = NULL ;
     firstBlock->prev = NULL ;
 }
@@ -116,10 +130,23 @@ void *mem_alloc_standard_pool(mem_pool_t *pool, size_t size) {
     #ENDIFDEF
 }
 
-void mem_free_standard_pool(mem_pool_t *pool, void *addr)
-{
-    /* TO BE IMPLEMENTED */
-    printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+
+
+void mem_free_standard_pool(mem_pool_t *pool, void *addr){
+
+    mem_std_free_block_t* free_block = (mem_std_free_block_t*) addr;
+    set_block_free(&free_block->header);
+    
+    mem_std_free_block_t* curr = (mem_std_free_block_t *)pool->first_free;
+
+    while (curr->next < addr){
+        curr = curr->next;
+    }
+
+    mem_std_free_block_t* tmp_next = curr->next;
+    curr->next = free_block;
+    free_block->prev = curr;
+    free_block->next = tmp_next;    
 }
 
 size_t mem_get_allocated_block_size_standard_pool(mem_pool_t *pool, void *addr)
