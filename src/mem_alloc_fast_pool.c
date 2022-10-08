ab52299f6879b1630fdc5822ebbf51c434baf222
ErwanLorteau <87809072+ErwanLorteau@users.noreply.github.com>
2022-10-08 14:51:58 +0200

    Commenting several functions in mem_alloc_fast_pool.c and mem_alloc_standard_pool.c

--- src/mem_alloc_fast_pool.c
+++ src/mem_alloc_fast_pool.c
@@ -5,16 +5,23 @@
 #include "my_mmap.h"
 #include "mem_alloc.h"
 
-void init_fast_pool(mem_pool_t *p, size_t size, size_t min_request_size, size_t max_request_size)
-{
-    char* region = (char *)my_mmap(size);
 
+/**
+ *
+ * @param p a pointer to the mem_pool
+ * @param the size of the pool to allocate
+ * @param min_request_size : minimal amount of payload required be to allocated in this pool
+ * @param max_request_size maximal amount of payload required be to allocated in this pool
+ */
+void init_fast_pool(mem_pool_t *p, size_t size, size_t min_request_size, size_t max_request_size) {
+
+    //Initializing the pool
+    char* region = (char *)my_mmap(size);
     p->start = region;
     p->end = region+size-1;
     p->first_free = region;
 
-    //printf("min: %d, max: %d\n",min_request_size, max_request_size);
-
+    //Initializing the list of free-block (each block have the same size (max_request-size)
     for(int i=0; i<size; i+=max_request_size){
         if (i+max_request_size <size){
             ((mem_fast_free_block_t *)&region[i])->next = (mem_fast_free_block_t *)&region[i+max_request_size];
@@ -31,11 +38,19 @@ void init_fast_pool(mem_pool_t *p, size_t size, size_t min_request_size, size_t 
     }    
 }
 
-void *mem_alloc_fast_pool(mem_pool_t *pool, size_t size)
-{
+/**
+ * Allocate in a fast pool
+ * @param pool : The pool where the memory is allocated
+ * @param size : Size to allocate
+ * @return a pointer to the returned block
+ */
+void *mem_alloc_fast_pool(mem_pool_t *pool, size_t size) {
+
+    //Guards
     if (size > pool->max_request_size)
         return NULL;
-    
+
+    //Updating the list of free block.
     void* result = pool->first_free;
     mem_fast_free_block_t*  next = ((mem_fast_free_block_t *)pool->first_free)->next;
     pool->first_free = (void *)next;
@@ -50,15 +65,26 @@ void *mem_alloc_fast_pool(mem_pool_t *pool, size_t size)
     // }
 }
 
-void mem_free_fast_pool(mem_pool_t *pool, void *b)
-{
+/**
+ * Free an allocated block
+ * @param pool the pool where the block is
+ * @param a pointer to the block
+ */
+void mem_free_fast_pool(mem_pool_t *pool, void *b) {
+
+    //Replacing the block in the first index of the free block list
     mem_fast_free_block_t* next = (mem_fast_free_block_t*) pool->first_free;
     ((mem_fast_free_block_t*) b)->next = next;
     pool->first_free = b;
 }
 
-size_t mem_get_allocated_block_size_fast_pool(mem_pool_t *pool, void *addr)
-{
+/**
+ * Return the size of a block.
+ * @param pool the pool where the block is
+ * @param addr the adress of the block
+ * @return the size of the block
+ */
+size_t mem_get_allocated_block_size_fast_pool(mem_pool_t *pool, void *addr) {
     size_t res;
     res = pool->max_request_size;
     return res;
